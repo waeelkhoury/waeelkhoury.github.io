bdbe56e02f4349c46e04c6e2e8e9b4045e88363f
waeelkhoury <waeelkhoury6@gmail.com>
2024-03-11 11:03:34 +0200

    gg

---
+++ eighthjava.js
@@ -0,0 +1,7 @@
+console.log("Script loaded successfully!");
+
+function divClicked() {
+    var dorothyElement = document.getElementById('dorothy');
+    dorothyElement.style.left = '300px';
+    dorothyElement.style.top = '400px';
+}
